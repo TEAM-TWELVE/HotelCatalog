f722068c5233cbd487953a2ac74149fd31263990
Highyard <benjaminoest@hotmail.com>
2020-12-12 15:19:13 +0100

    Added URLEncoder

--- src/main/java/com/example/demo/controller/HotelController.java
+++ src/main/java/com/example/demo/controller/HotelController.java
@@ -5,6 +5,8 @@ import com.example.demo.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
@@ -16,7 +18,7 @@ public class HotelController {
 
     @GetMapping("/{city}")
     public List<Hotel> findByCity(@PathVariable String city) throws Exception {
-        var hotels = hotelService.findByCity(city);
+        var hotels = hotelService.findByCity(URLDecoder.decode(city, StandardCharsets.UTF_8));
         if(!hotels.isEmpty()){
             return hotels;
         }else {
